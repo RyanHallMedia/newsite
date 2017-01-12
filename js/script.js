b20f8ff89e55803a12796d05ae2db3895c3c87ad
Ryan Hall <ryan@theorythree.com>
2017-01-11 18:17:57 -0600

    Pretty damn good start

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 	// for animating nav
 	var nav = $(".startnav");
 	$(window).scroll(function() {
-			var $height = $('header').height() - 65;
+			var $height = $('header').height() - $('.info').height();
 			var scroll = $(window).scrollTop();
 
 			if (scroll >= $height) {
@@ -15,16 +15,15 @@ $(document).ready(function(){
 	});
 
 
-
 	//mobile nav
 	$('.navbutton').on(hitEvent, function() {
-	    $('nav').toggleClass('nav-open');
+	    $('.side-nav').toggleClass('nav-open');
 			$('.navbutton').toggleClass('nav-open');
 			$('.startnav').toggleClass('nav-open');
 	});
 	//mobile nav
 	$('.mobilenav li a').on(hitEvent, function() {
-	    $('nav').toggleClass('nav-open');
+	    $('.side-nav').toggleClass('nav-open');
 			$('.navbutton').toggleClass('nav-open');
 			$('.startnav').toggleClass('nav-open');
 	});
@@ -38,40 +37,100 @@ $(document).ready(function(){
 
 
 
+	//work section height
+	var limit1 = $('.one .lower').height() + $('.one h4').height();
+	$('.workinfo.one').height(limit1);
+
+	var limit12 = $('.one h4').height() + 10;
+	$('.one .lower').css("top", limit12);
+
+
+	//work section height
+	var limit2 = $('.two .lower').height() + $('.two h4').height();
+	$('.workinfo.two').height(limit2);
+
+	var limit22 = $('.two h4').height() + 10;
+	$('.two .lower').css("top", limit22);
+
+
+
+	//work section height
+	var limit3 = $('.three .lower').height() + $('.three h4').height();
+	$('.workinfo.three').height(limit3);
+
+	var limit32 = $('.three h4').height() + 10;
+	$('.three .lower').css("top", limit32);
+
+
+
+	//work section height
+	var limit4 = $('.four .lower').height() + $('.four h4').height();
+	$('.workinfo.four').height(limit4);
+
+	var limit42 = $('.four h4').height() + 10;
+	$('.four .lower').css("top", limit42);
+
+
+
+	//work section height
+	var limit5 = $('.five .lower').height() + $('.five h4').height();
+	$('.workinfo.five').height(limit5);
+
+	var limit52 = $('.five h4').height() + 10;
+	$('.five .lower').css("top", limit52);
+
+
+
+
+
+
   //sets animations on pageload
 	setTimeout(function(){
-    $('.body').addClass('animated fadeIn');
-		$('.body').removeClass("preanimate");
-	}, 100);
-	setTimeout(function(){
-    $('header .subheader').addClass('animated fadeIn');
-		$('header .subheader').removeClass("preanimate");
+		$('.bigtext h1').addClass('animated fadeIn');
+		$('.bigtext h1').removeClass("preanimate");
 	}, 300);
 	setTimeout(function(){
-		$('header h1').addClass('animated fadeIn');
-		$('header h1').removeClass("preanimate");
-	}, 400);
+		$('.linediv').addClass('animated fadeInDown');
+		$('.linediv').removeClass("preanimate");
+	}, 600);
 	setTimeout(function(){
-		$('header h5').addClass('animated fadeIn');
-		$('header h5').removeClass("preanimate");
-	}, 800);
+		$('.is-overlay').addClass('animated fadeIn border');
+		$('.is-overlay').removeClass("preanimate");
+	}, 900);
 	setTimeout(function(){
-		$('header .play').addClass('animated fadeIn');
-		$('header .play').removeClass("preanimate");
-	}, 1000);
+		$('.info h2').addClass('animated fadeIn');
+		$('.info h2').removeClass("preanimate");
+	}, 1400);
 	setTimeout(function(){
-		$('nav').addClass('animated fadeInDown');
-		$('nav').removeClass("preanimate");
-	}, 1000);
+		$('.info p').addClass('animated fadeIn');
+		$('.info p').removeClass("preanimate");
+	}, 1800);
 	setTimeout(function(){
-		$('header .scroll').addClass('animated fadeIn');
-		$('header .scroll').removeClass("preanimate");
-	}, 1600);
+		$('.info a').addClass('animated fadeIn');
+		$('.info a').removeClass("preanimate");
+	}, 2000);
+	setTimeout(function(){
+    $('nav ul').addClass('animated fadeInDown');
+		$('nav ul').removeClass("preanimate");
+	}, 2400);
+	setTimeout(function(){
+		$('.email span').addClass('animated fadeInRight');
+		$('.email span').removeClass("preanimate");
+	}, 2400);
+	setTimeout(function(){
+		$('.nav-group .navigation').addClass('animated fadeInDown');
+		$('.nav-group .navigation').removeClass("preanimate");
+	}, 2800);
+	setTimeout(function(){
+		$('.linedown').addClass('animated fadeInDown');
+		$('.linedown').removeClass("preanimate");
+	}, 3600);
+
 
   //animations on scroll
   var appear = $(window).height() - 50;
   $(window).scroll(function() {
-    $('.workgrid h2, .contact-from h2').each(function(){
+    $('.workgrid h2').each(function(){
       var imagePos = $(this).offset().top;
       var topOfWindow = $(window).scrollTop();
 
